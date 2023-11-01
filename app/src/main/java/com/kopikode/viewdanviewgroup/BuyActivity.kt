cafbc9fdf9a3b9ca8e55ca95428ac446a4f24038
KOPI KODE <junadhi@sar.ac.id>
2023-11-01 11:58:49 +0700

    Update intent

---
+++ app/src/main/java/com/kopikode/viewdanviewgroup/BuyActivity.kt
@@ -0,0 +1,13 @@
+package com.kopikode.viewdanviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy)
+
+
+    }
+}
--- app/src/main/java/com/kopikode/viewdanviewgroup/MainActivity.kt
+++ app/src/main/java/com/kopikode/viewdanviewgroup/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.kopikode.viewdanviewgroup
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnBuy = findViewById<Button>(R.id.button_buy)
+        btnBuy.setOnClickListener(this)
+
+        val btnDial = findViewById<Button>(R.id.button_call)
+        btnDial.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button_buy -> {
+                val intentBuy = Intent(this@MainActivity, BuyActivity::class.java)
+                startActivity(intentBuy)
+            }
+
+            R.id.button_call -> {
+                val phoneNumber = "0823474849494"
+                val dialNumber = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialNumber)
+            }
+        }
     }
 }
